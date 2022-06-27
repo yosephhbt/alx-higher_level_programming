ad2539d32d7e0c7e7137367a25a3d90bdbe0b7a4
yosephhbt <josephhbt@gmail.com>
2022-06-27 11:43:05 -0700

    7-edges

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word[0:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
+printf("First 3 letters:{}".format(word_first_3))
+printf("Last 2 letter: {}".format(word_last_2))
+printf("Middle word: {}".format(middle_word))
