18185827255537f90135c95d51ec4950c6fa93bb
Yoseph Habte <yWX1054903@china.huawei.com>
2022-07-06 20:35:43 +0300

    more data structure

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    maxv = max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
+    return (maxv)
+    
---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    s = set(my_list)
+    add = sum(s)
+    return (add)
+    
