8fee856120fd0db14730b52bbb43b76d471aaaec
Yoseph Habte (A) <y84254832@china.huawei.com>
2022-07-28 22:25:56 +0300

    more class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+
+"""Defines a locked class."""
+
+
 class LockedClass:
-    """ Class without __dict__ and locked attributes """
-    __slots__ = "first_name"
+    """
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
+
+    __slots__ = ["first_name"]
