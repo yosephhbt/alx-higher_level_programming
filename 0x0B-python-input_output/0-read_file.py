d8d64fa21f5746376cd6c6752380a9064727943c
Yoseph Habte (A) <y84254832@china.huawei.com>
2022-08-14 16:22:16 +0300

    input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+"""Read file"""
 
 
 def read_file(filename=""):
-    """ function that reads a text file (UTF8) and prints it to stdout """
+    """a function that reads a text file (UTF8)
+    and prints it to stdout
 
-    with open(filename, encoding="utf-8") as myfile:
-        print(myfile.read(), end='')
+    Args:
+        filename(any): name of the file
+
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
--- 0x0B-python-input_output/README.md
+++ 0x0B-python-input_output/README.md
@@ -1,1 +1,1 @@
-README
+# Python - Input/Output
